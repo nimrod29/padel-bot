b6b1010fb928d09898d8b1f7d1f7ff79dc3fa60b
Nimrod Shanny <you@example.com>
2025-07-17 17:48:15 +0300

    Changed from 18:30 to start from 18:00

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ MIN_CONSECUTIVE_SPOTS = 3  # Minimum consecutive free spots (1.5 hours)
 ALTERNATIVE_MIN_SPOTS = 2  # Alternative minimum spots (1 hour)
 
 # Time range for monitoring (in 24-hour format)
-MONITOR_START_TIME = "18:30"  # 6:30 PM
+MONITOR_START_TIME = "18:00"  # 6:30 PM
 MONITOR_END_TIME = "00:00"  # 12:00 AM
 
 # Facility IDs for different locations
